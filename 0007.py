b85a97817b1f0a76cbd0b899ae15fbfc4d3805fd
Luis Aguiar <80652939+theseeds@users.noreply.github.com>
2021-04-27 09:00:01 -0300

    Add files via upload

---
+++ 0007.py
@@ -0,0 +1,5 @@
+n = input('Digite qualquer coisa:')
+print(n.isalnum())
+# Poderia ser .isalpha, .isnumeric, .isidentifier, etc... pycharm mostra.
+# São muitos (.is)
+
---
+++ 0008.py
@@ -0,0 +1,16 @@
+a = input("Digite qualquer coisa:\n")
+print('O que você digitou é do tipo:',type(a))
+print('Só tem espaços?', a.isspace())
+print('É um número?', a.isnumeric())
+print('É alfabético?', a.isalpha())
+print('Só tem maiúsculas?', a.isupper())
+print('Só tem minúsculas?', a.islower())
+print('É alfanumérico?', a.isalnum())
+print('Está capitalizada?', a.istitle())
+
+# O a é chamado de objeto. Cada objeto tem características e realiza funcionalidades.
+
+# Existem atributos e existem métodos.
+# Todos os .is() são considerados métodos. Os métodos são as ações que um objeto pode realizar.
+# Atributos são as características de um objeto, essas características também são conhecidas como variáveis.
+# Utilizando o exemplo dos cães, temos alguns atributos, tais como: cor, peso, altura e nome.
